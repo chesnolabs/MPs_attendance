d4b9eedce887a9ec03796391507877bf8b0c67d0
n-mouse <murahoid@gmail.com>
2014-12-10 14:00:46 +0200

    new life

--- attendance.rb
+++ attendance.rb
@@ -5,8 +5,8 @@ require 'mechanize'
 require 'date'
 
 
-kodsall = *(2..486)
-kods = kodsall.delete_if { |ko| [397, 230, 287, 220, 231, 228, 309, 265, 222, 313, 332, 418, 327, 312, 132, 440, 422, 356, 310, 341, 336, 435, 84, 427, 292, 462, 466].include? ko }
+kodsall = *(1..435)
+kods = kodsall.delete_if { |ko| [245,317,401,407].include? ko }
 bdate = if ARGV[0] 
    ARGV[0]
  else 
@@ -22,21 +22,24 @@ arr = []
 
 kods.each do |kod|
   agent = Mechanize.new
+  begin
   page = agent.get("http://w1.c1.rada.gov.ua/pls/radan_gs09/ns_dep_reg_w_list_print?startDate="+bdate+"&endDate="+edate+"&kod="+kod.to_s)
   arr[0] = page.search('tr')[1].text.split(/\n/)[1]
   if page.search('tr')[3..-1]!=nil
     page.search('tr')[3..-1].each do |tr|
       a = tr.text.split(/\n/)
       arr[1] = a[2]
-      arr[2] = a[4]
-      arr[3] = a[7]
+      arr[2] = a[5]
+      arr[3] = a[8]
         CSV.open("register.csv", "ab", {:col_sep => "\t"}) do |data|
         data << arr
       end
     end 
-
   end
   sleep 1
+  rescue
+    next
+  end
 end
 
 
